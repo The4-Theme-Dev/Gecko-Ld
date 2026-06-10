fe2a7d1a36f328aa17c72335523bc436766e6064
hekichine <chienlx1005@gmail.com>
2026-06-10 15:17:13 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,6 +60,9 @@ class bannerPopup extends HTMLElement{
           opacity: 0;
           transition: .2s ease-in-out;
           cursor: url('./assets/images/cursor-close.svg') 25 25, auto;
+          @media (width > 1150px){
+            display: none;
+          }
         }
         .content{
 
@@ -74,6 +77,9 @@ class bannerPopup extends HTMLElement{
           // aspect-ratio: 1;
           border-radius: 10px;
           overflow:hidden;
+          @media (width > 1150px){
+            width: min(400px, 30vw);
+          }
         }
         button.close{
           position: absolute;
@@ -126,7 +132,7 @@ class bannerPopup extends HTMLElement{
     this.style.setProperty('display','flex');
     setTimeout(() => {      
       this.overlay.style.setProperty('opacity','1');
-      document.querySelector('html').style.setProperty('overflow','hidden');
+      // document.querySelector('html').style.setProperty('overflow','hidden');
     }, 300);
     setTimeout(() => {
       this.content.style.setProperty('transform','translate(0,0)');
@@ -138,7 +144,7 @@ class bannerPopup extends HTMLElement{
     this.content.style.setProperty('opacity','0');
     setTimeout(() => {      
       this.overlay.style.setProperty('opacity','0');
-      document.querySelector('html').style.removeProperty('overflow');
+      // document.querySelector('html').style.removeProperty('overflow');
     }, 300);
     setTimeout(() => {
       this.style.setProperty('display','none');
